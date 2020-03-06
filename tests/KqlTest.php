03b86f6365186aa05e32397d26c1bf209e331c98
Bruno Meilick <b@bnomei.com>
2020-03-06 20:05:53 +0000

    testing
    - basic unittest setup
    - basic auth user & config.php
    - imported starterkit for testing
    
    Signed-off-by: Bruno Meilick <b@bnomei.com>

---
+++ tests/KqlTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class KqlTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+
+    }
+
+}
---
+++ tests/site/plugins/kql/index.php
@@ -0,0 +1,3 @@
+<?php
+
+@include_once realpath(__DIR__ . '/../../../../')  . '/index.php';
